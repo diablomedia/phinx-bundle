c1b885ca0868f05a023f69ebb78e0e0e8a199051
Jay Klehr <jay@diablomedia.com>
2026-07-28 15:59:13 -0600

    adding code quality tools

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__ . '/src');
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'declare_strict_types' => true,
+    ])
+    ->setFinder($finder)
+    ->setCacheFile(__DIR__ . '/var/php-cs-fixer.cache');
